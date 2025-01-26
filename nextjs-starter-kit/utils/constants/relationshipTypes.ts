625f4cd9df897e891739f1b8707756d01c58dac1
David <dzach726@gmail.com>
2025-01-26 13:49:50 -0600

    change name to frontend

--- nextjs-starter-kit/utils/constants/relationshipTypes.ts
+++
@@ -1,8 +0,0 @@
-export const relationshipTypes: { [key: string]: string } = {
-  '0': 'Prior Registration',
-  '1': 'Continuity Parent',
-  '2': 'Continuity Child',
-  '3': 'Non Registration Document Control Number'
-}
-
-export default relationshipTypes 
